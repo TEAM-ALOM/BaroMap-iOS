c592a3e6b0443b16023568361eaee355af5e1ffc
thissound22 <22011837@sju.ac.kr>
2023-11-21 07:38:38 +0900

    feat: 프로젝트 생성

---
+++ BaroMap/BaroMap/BaroMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaroMapApp.swift
+//  BaroMap
+//
+//  Created by 이소리 on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BaroMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
