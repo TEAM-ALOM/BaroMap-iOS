2ed61fb74d177968e398b4db4fdfbe4f5ef26b69
thissound22 <22011837@sju.ac.kr>
2023-12-29 17:44:22 +0900

    feat: SearchLocationStore+View

--- BaroMap/BaroMap/Modules/Feature/Map/SearchLocationStore.swift
+++ BaroMap/BaroMap/Modules/Feature/Map/SearchLocationStore.swift
@@ -1,8 +1,56 @@
 //
 //  SearchLocationStore.swift
-//  BaroMap
+//  classPractice
 //
-//  Created by 이소리 on 12/4/23.
+//  Created by 이소리 on 11/10/23.
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct SearchLocationStore: Reducer {
+    struct State: Equatable {
+        @PresentationState var isShownSearchDestinationView: SearchDestinationStore.State?
+        
+        var isDefectedArrowButtonVisible: Bool = true
+    }
+    
+    enum Action: Equatable {
+        case searchLocationButtonTapped
+        case zoomInButtonTapped
+        case zoomOutButtonTapped
+        case getDirectionsButtonTapped
+        case currentLocationButtonTapped
+        
+        case searching(PresentationAction<SearchDestinationStore.Action>)
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .searchLocationButtonTapped:
+                state.isShownSearchDestinationView = SearchDestinationStore.State() // 상태를 초기화
+                return .none
+                
+            case .zoomInButtonTapped:
+                return .none
+                
+            case .zoomOutButtonTapped:
+                return .none
+                
+            case .getDirectionsButtonTapped:
+                state.isDefectedArrowButtonVisible.toggle()
+                return .none
+                
+            case .currentLocationButtonTapped:
+                return .none
+                
+            case .searching: // 검색 중에 수행되는 일
+                return .none
+            }
+        }
+        .ifLet(\.$isShownSearchDestinationView, action: /Action.searching) {
+            SearchDestinationStore()
+        }
+    }
+}
