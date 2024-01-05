7095b62a599f496e1f7e7f545935f6c0ff36d041
김휘선 <hwiseon_9151@naver.com>
2024-01-06 01:59:12 +0900

    직전 커밋과 같은 내용

--- BaroMap/BaroMap/Modules/Sources/Feature/Home/tabView.swift
+++ BaroMap/BaroMap/Modules/Sources/Feature/Home/tabView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct tabView: View {
+    @State private var selection = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection : $selection){
+            tabView_home2()
+                .tabItem{
+                    Image(systemName:"house.fill")
+                    Text("홈")
+                }
+                .tag(0)
+            
+            Text("지도")
+                .font(.system(size:30))
+                .tabItem{
+                    Image(systemName:"map.fill")
+                    Text("지도")
+                }
+                .tag(1)
+            
+            Text("메뉴")
+                .font(.system(size:30))
+                .tabItem{
+                    Image(systemName:"line.3.horizontal")
+                    Text("메뉴")
+                }
+                .tag(2)
+        }.accentColor(.green)
     }
 }
 
 #Preview {
     tabView()
 }
+
--- BaroMap/BaroMap/Modules/Sources/Feature/Home/tabView_home2.swift
+++ BaroMap/BaroMap/Modules/Sources/Feature/Home/tabView_home2.swift
@@ -9,7 +9,245 @@ import SwiftUI
 
 struct tabView_home2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                VStack(spacing:20){
+                    Text("안테나")
+                        .font(.system(size:34))
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                    Text("현재 경로")
+                        .fontWeight(.bold)
+                }
+                Spacer()
+            }
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 353, height: 432)
+                    .background(.white)
+                    .cornerRadius(20)
+                    .shadow(color: .black.opacity(0.1), radius: 2.5, x: 0, y: 0)
+                VStack{
+                    HStack{
+                        Text("21:59 도착")
+                            .font(.system(size:20))
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("18분 남음")
+                            .font(.system(size:20))
+                            .fontWeight(.bold)
+                    }
+                    HStack{
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .frame(width: 12, height: 12)
+                            .foregroundColor(.red)
+                            .padding(.leading,3)
+                        Text("세종대학교")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    HStack{
+//                        VStack{
+//                            Image(systemName: "figure.walk")
+//                                .resizable()
+//                                .frame(width: 12, height: 12)
+//                                .foregroundColor(.brown)
+//                            Image(systemName:"arrowtriangle.down.fill")
+//                                .resizable()
+//                                .frame(width: 8, height: 8)
+//                                .foregroundColor(.brown)
+//                        }
+                        Image(systemName:"arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 8, height: 8)
+                            .foregroundColor(.brown)
+                            .padding(.leading, 5)
+                        Text("걷기")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("도보 274m, 5분 소요")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "7.circle.fill")
+                            .resizable()
+                            .frame(width:20, height:20)
+                            .foregroundColor(.brown)
+                        Text("어린이대공원역 승차")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("석남(거북시장) 방면")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width:8, height:8)
+                            .foregroundColor(.brown)
+                            .padding(.leading, 5)
+                        Text("빠른 환승 1-1")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width:8, height:8)
+                            .foregroundColor(.brown)
+                            .padding(.leading, 5)
+                        Text("2분")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Text("1개역 이동")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "7.circle.fill")
+                            .resizable()
+                            .frame(width:20, height:20)
+                            .foregroundColor(.brown)
+                        Text("건대입구역 하차")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("8번 출구")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+//                        VStack{
+//                            Image(systemName: "figure.walk")
+//                                .resizable()
+//                                .frame(width: 12, height: 12)
+//                                .foregroundColor(.brown)
+//                            Image(systemName:"arrowtriangle.down.fill")
+//                                .resizable()
+//                                .frame(width: 8, height: 8)
+//                                .foregroundColor(.brown)
+//                                .padding(.leading, 2)
+//                        }
+                        Image(systemName:"arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 8, height: 8)
+                            .foregroundColor(.brown)
+                            .padding(.leading, 5)
+                        Text("걷기")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("도보 274m, 5분 소요")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "2.circle.fill")
+                            .resizable()
+                            .frame(width:20, height:20)
+                            .foregroundColor(.green)
+                        Text("건대입구역 승차")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("구의 방면")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width:8, height:8)
+                            .foregroundColor(.green)
+                            .padding(.leading, 5)
+                        Text("빠른 하차 3-4, 9-3")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width:8, height:8)
+                            .foregroundColor(.green)
+                            .padding(.leading, 5)
+                        Text("13분")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        
+                        Text("6개역 이동")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                        Image(systemName: "chevron.down")
+                        //누르면 역 리스트로 뜨게끔 하기
+                            .resizable()
+                            .frame(width:8, height: 4)
+                            .foregroundColor(.brown)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "2.circle.fill")
+                            .resizable()
+                            .frame(width:20, height:20)
+                            .foregroundColor(.green)
+                        Text("종합운동장역 하차")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("8번 출구")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName:"arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 8, height: 8)
+                            .foregroundColor(.green)
+                            .padding(.leading, 5)
+                        Text("걷기")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Text("도보 274m, 5분 소요")
+                            .font(.system(size:11))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    HStack{
+                        Image(systemName: "circle.circle.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.red)
+
+                        Text("종합운동장역 2호선 도착")
+                            .font(.system(size:13))
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                }
+                .padding()
+            }
+            Spacer()
+        }
+        .padding()
     }
 }
 
